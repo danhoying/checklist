d348bc9960fe3f6212c66f9cd7b83ccd773682bb
Dan Hoying <danhoying@gmail.com>
2016-02-08 11:25:13 -0800

    Add styling

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -40,7 +40,7 @@ class ItemsController < ApplicationController
   end
 
   def complete
-    @item = Item.find(params(:id))
+    @item = Item.find(params[:id])
     @item.update_attribute(:completed_at, Time.now)
     redirect_to root_path
   end
